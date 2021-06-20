6bf4595e3ceba8517d8db890393cb5d840652160
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-06-19 23:51:58 -0400

    added resume

--- portfolio/src/portfolioData.js
+++ portfolio/src/portfolioData.js
@@ -13,7 +13,7 @@ const projects = [
         "id": "project1",
         "name":"Just Your Type",
         "description":"Just Your Type was born out of a desire to reinvent and gamify how designers, like you, find fonts. Playing off the dating app model, this application allows you to quickly save the fonts that are <strong>JUST YOUR TYPE</strong> and create a library of your favorite typography.",
-        "imgurl":"assets/just-your-type.png",
+        "imgurl": "/assets/just-your-type.png",
         "skills": ["HTML, CSS, JS"],
         "url": "https://hanlee-311.github.io/Just-Your-Type/"
       },
@@ -21,7 +21,7 @@ const projects = [
         "id":"project2",
         "name":"Calendar App",
         "description":"A simple calendar application that allows the user to add events/tasks for each hour of the day. Each hour is color coded based on the current time. The events/tasks added by the user will be available to the user after a page refresh. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery and Moment.js. It was built with HTML, CSS, Bootstrap, Javascript, JQuery, and Moment.js",
-        "imgurl":"assets/day-planner-preview.png",
+        "imgurl":"/assets/day-planner-preview.png",
         "skills": ["HTML, CSS, JS, JQuery, Moment.js"],
         "url":"https://barbarashea.github.io/calendar/"
       },
@@ -29,7 +29,7 @@ const projects = [
         "id":"project3",
         "name":"Fitness Tracker",
         "description":"This application allows the user to track their resistance and cardio workouts, including distance, duration, reps, weight, and sets.",
-        "imgurl":"assets/workout-tracker.png",
+        "imgurl":"/assets/workout-tracker.png",
         "skills": ["HTML, CSS, JS, MongoDB, Mongoose"],
         "url": "https://workout-tracker-bsk.herokuapp.com/"
       },
@@ -37,7 +37,7 @@ const projects = [
         "id":"project4",
         "name":"Budget Tracker",
         "description":"This simple budget tracker uses PWA technologies to allow for offline funtionality. Users are able to record deposits and withdrawals both online and offline.",
-        "imgurl":"assets/budget-app.png",
+        "imgurl":"/assets/budget-app.png",
         "skills": ["HTML, CSS, JS, IndexDB, Mongoose"],
         "url": "https://budget-tracker-bsk.herokuapp.com/"
       },
@@ -47,7 +47,7 @@ const social = {
   github: "https://github.com/BarbaraShea",
   linkedin: "https://linkedin.com/BarbaraShea",
   instagram: "https://instagram.com/babsk",
-  resume: "",
+  resume: "/assets/resume.pdf",
 };
 
 const education = [
